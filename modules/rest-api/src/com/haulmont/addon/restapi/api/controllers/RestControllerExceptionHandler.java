555f490baa8922d7f777224845e2b0e402c1c6ad
Nikita Shchienko <shchienko@haulmont.com>
2019-10-28 15:53:30 +0400

    Future and Past validations have wrong invalidValue in the response body in the REST API #40
    (cherry picked from commit 3822a4732d71eade96f2769da377aa15cb4dc84a)

--- modules/rest-api/src/com/haulmont/addon/restapi/api/controllers/RestControllerExceptionHandler.java
+++ modules/rest-api/src/com/haulmont/addon/restapi/api/controllers/RestControllerExceptionHandler.java
@@ -19,8 +19,14 @@ package com.haulmont.addon.restapi.api.controllers;
 import com.haulmont.addon.restapi.api.exception.ConstraintViolationInfo;
 import com.haulmont.addon.restapi.api.exception.ErrorInfo;
 import com.haulmont.addon.restapi.api.exception.RestAPIException;
+import com.haulmont.chile.core.datatypes.Datatype;
+import com.haulmont.chile.core.datatypes.Datatypes;
+import com.haulmont.chile.core.model.MetaClass;
+import com.haulmont.chile.core.model.MetaPropertyPath;
+import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.core.global.RemoteException;
 import com.haulmont.cuba.core.global.RowLevelSecurityException;
+import com.haulmont.cuba.core.global.UserSessionSource;
 import com.haulmont.cuba.core.global.validation.CustomValidationException;
 import com.haulmont.cuba.core.global.validation.MethodParametersValidationException;
 import com.haulmont.cuba.core.global.validation.MethodResultValidationException;
@@ -33,6 +39,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.inject.Inject;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
@@ -45,6 +52,12 @@ public class RestControllerExceptionHandler {
 
     protected static final Collection<Class> SERIALIZABLE_INVALID_VALUE_TYPES =
             Arrays.asList(String.class, Date.class, Number.class, Enum.class, UUID.class);
+
+    @Inject
+    protected UserSessionSource userSessionSource;
+
+    @Inject
+    protected Metadata metadata;
 
     @ExceptionHandler(RestAPIException.class)
     @ResponseBody
@@ -159,7 +172,12 @@ public class RestControllerExceptionHandler {
                     }
                 }
                 if (serializable) {
-                    info.setInvalidValue(invalidValue);
+                    if (invalidValue instanceof Date) {
+                        Datatype datatype = getDatatype(violation);
+                        info.setInvalidValue(datatype.format(invalidValue, userSessionSource.getLocale()));
+                    } else {
+                        info.setInvalidValue(invalidValue);
+                    }
                 } else {
                     info.setInvalidValue(null);
                 }
@@ -172,5 +190,14 @@ public class RestControllerExceptionHandler {
             violationInfos.add(info);
         }
         return violationInfos;
+    }
+
+    protected Datatype getDatatype(ConstraintViolation<?> violation) {
+        MetaClass metaClass = metadata.getClass(violation.getRootBeanClass());
+        String propertyPath = violation.getPropertyPath().toString();
+        MetaPropertyPath metaPropertyPath = metadata.getTools().resolveMetaPropertyPath(metaClass, propertyPath);
+        return metaPropertyPath == null
+                ? Datatypes.get(Date.class)
+                : metaPropertyPath.getRange().asDatatype();
     }
 }
